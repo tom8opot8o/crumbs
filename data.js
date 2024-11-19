de5ae5c720f1e4b5b32aa18b879fb1ecea265b09
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2024-11-19 21:36:27 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const pageData = {
+    accountName: "thestefantorres",
+    posts: [
+        {
+            image: "image1.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https://tom8opot8o.github.io/crumbs&aff_id=5295&offer_id=1&content_id=0c89e930-cf8c-4bae-af8c-eb91a279ea90"
+        },
+        {
+            image: "image2.jpg",
+            link: "https://www.example2.com"
+        },
+        {
+            image: "image3.jpg",
+            link: "https://www.example3.com"
+        }
+    ]
+};
