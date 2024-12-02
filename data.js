288283027087605a59d63de4892082038d134327
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2024-12-02 05:06:44 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,8 +2,8 @@ const pageData = {
     accountName: "miranda_stayclassy",
     posts: [
         {
-            image: "image1.jpg",
-            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=c6b63c73-aba9-40cf-a240-94826e1372ff&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+            image: "image.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=ccc0b4f5-e983-4d29-819d-87c54c5165e3&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
         }
     ]
 };
