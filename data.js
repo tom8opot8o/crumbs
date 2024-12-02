29206073f82bd62d32cb114d214b63becc176d4f
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2024-12-02 04:41:45 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 const pageData = {
-    accountName: "thestefantorres",
+    accountName: "miranda_stayclassy",
     posts: [
         {
             image: "image1.jpg",
-            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fthestefantorres-tuna-fish-sandwich%2F&aff_id=5295&offer_id=1&content_id=f80a0470-8892-4cea-a71a-06d597f3dd15&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=c6b63c73-aba9-40cf-a240-94826e1372ff&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
         },
         {
-            image: "image2.jpg",
+            image: "",
             link: "https://www.example2.com"
         },
         {
-            image: "image3.jpg",
+            image: "",
             link: "https://www.example3.com"
         }
     ]
