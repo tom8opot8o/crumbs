5c2438e7f1e52f0eb0e482c9a281555dd55fce25
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2024-12-02 04:51:27 +0100

    Update data.js

--- data.js
+++ data.js
@@ -4,14 +4,6 @@ const pageData = {
         {
             image: "image1.jpg",
             link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=c6b63c73-aba9-40cf-a240-94826e1372ff&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
-        },
-        {
-            image: "image1.jpg",
-            link: "https://www.example2.com"
-        },
-        {
-            image: "image2.jpg",
-            link: "https://www.example3.com"
         }
     ]
 };
