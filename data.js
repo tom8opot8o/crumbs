f4a6820ae35b29e0a0acfb815e8d6a717d20bc25
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2024-12-02 05:24:54 +0100

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const pageData = {
     posts: [
         {
             image: "image1.jpg",
-            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=ccc0b4f5-e983-4d29-819d-87c54c5165e3&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=f27f7bf7-4cbd-4cf2-87b3-0bd89da81241&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
         },
         {
             image: "image2.jpg",
